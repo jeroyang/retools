572653c740e5cabaf721d8031f6a1545ea1c2bea
jeroyang <jeroyang@gmail.com>
2015-08-21 23:29:11 +0800

    change import statement

--- retools/__init__.py
+++ retools/__init__.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
+from .retools import *
 
 __author__ = 'Chia-Jung, Yang'
 __email__ = 'jeroyang@gmail.com'
 __version__ = '0.1.0'
+
+
